91ac05ec1312a146d638e8cb2df7bf027a2b2941
Lena Kwan <lenakwan@gmail.com>
2020-10-08 22:30:43 -0700

    server.js works and receives responses. script in serverTime.html might need to be split. issue with connecting the html file to the server, respones work if pasted in chrome inspector

--- COMP4537/labs/3/myModule.js
+++ COMP4537/labs/3/myModule.js
@@ -1,10 +1,4 @@
-let xhttp = new XMLHttpRequest();
-let str = "username";
-xhttp.open("GET", "http://localhost:8888/?name=" +str, true);
-xhttp.send();
-xhttp.onreadystatechange = function(){
-    if (this.readyState==4 && this.status == 200){
-        document.getElementById("greeting").innerHTML= "Hello" + this.responseText;
-        
-    }
-}
+
+exports.myDateTime = function() {
+    return Date();
+  }
--- COMP4537/labs/3/server.js
+++ COMP4537/labs/3/server.js
@@ -1,11 +1,12 @@
 let http = require('http');
 let url = require('url');
+let dt = require('./myModule');
+
 http.createServer(function (req, response){
     let q= url.parse(req.url, true);
-    response.write("Response's coming from the server...\n");
     console.log(q.query);
     response.writeHead(200, {'Content-Type': 'text/html', "Access-Control-Allow-Origin": '*'});
-    response.end('hello ' + q.query['name']);
-}).listen(8888);
+    response.end('hello ' + q.query['name'] + ' the current time is ' + dt.myDateTime());
+}).listen(8080);
 
 console.log('Server is running and listening...');
