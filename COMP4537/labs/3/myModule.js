0265cf63249ded2ba17ff826f667fe9b79dcaeab
Lena Kwan <lenakwan@gmail.com>
2020-10-09 16:24:58 -0700

    formatting added for date -> am/pm

--- COMP4537/labs/3/myModule.js
+++ COMP4537/labs/3/myModule.js
@@ -1,3 +1,11 @@
 exports.myDateTime = function() {
-  return Date();
+  let cur_date = new Date();
+  let hours = cur_date.getHours();
+  let minutes = cur_date.getMinutes();
+  console.log('time: ' + hours + minutes);
+  let ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours: 12;
+  minutes = minutes < 10 ? '0'+minutes : minutes; // append 0 if minutes <10
+  return hours + ':' + minutes + ' ' + ampm;
 }
