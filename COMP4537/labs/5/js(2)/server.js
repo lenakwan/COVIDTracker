15a472502ba9048a8960a2b04044332bb0178326
Lena Kwan <34289576+lenakwan@users.noreply.github.com>
2020-11-14 23:36:35 -0800

    finished backend with get/post methods

--- COMP4537/labs/5/js(2)/server.js
+++ COMP4537/labs/5/js(2)/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const { Client } = require('pg');
 const port = process.env.PORT || 3000;
 const app = express();
+const bodyParser = require('body-parser'); 
+
+
 
 const client = new Client({
     connectionString: 'postgres://tugjcevljpwyrz:24d2d08e09be41a7b763f56f3a2e9fd183169df6b4e4db978f8c83152926f958@ec2-3-233-236-188.compute-1.amazonaws.com:5432/daga8h5n112nup',
@@ -12,25 +15,45 @@ const client = new Client({
 
 client.connect();
 
-app.get("/createdb", (req, res) => {
-    client.query("CREATE DATABASE IF NOT EXISTS definitions", (err, result)=>{
-        if (err) throw err;
-        console.log(result);
-    })
-});
 
-app.get("/getdef", (req,res) =>{
+app.get("/getdef/:word", (req,res) =>{
+    const word = req.params.word;
+    console.log(word);
     client.query("SELECT * FROM definitions", (err, result) =>{
         if (err) throw err;
         console.log(result.rows);
+
+    for (resultword of result.rows) {
+        if (resultword.word === word) {
+            res.json(resultword.definition);
+            console.log('matches word');
+            return resultword.definition;
+        }
+    }
+
+    // Sending 404 when not found something is a good practice
+    res.status(404).send('Word not found');
     })
 })
 
-app.get("/adddef", (req,res) => {
-    client.query("INSERT INTO definitions (word, definition) VALUES ('testWord', 'testDefinition')", (err, result)=>{
+app.get("/getall", (req,res) =>{
+    client.query("SELECT * FROM definitions", (err, result) =>{
         if (err) throw err;
-        console.log(result);
+        console.log(result.rows);
+        res.json(result.rows);
+        return result.rows;
     })
+})
+
+app.use(bodyParser.json()); 
+app.post("/definition", (req,res) => {
+    let word = req.body.word;
+    let def = req.body.definition;
+    console.log("word = " +word+ "definition" +def);
+    client.query({
+        text: "INSERT INTO definitions (word, definition) VALUES($1, $2);",
+        values: [word, def]
+    });
 })
 
 
