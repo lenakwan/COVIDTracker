0ecac22b37493a3a0c74577202db22de88b70c83
Lena Kwan <lenakwan@gmail.com>
2020-12-05 01:23:33 -0800

    issues with singleflightsearch

--- COMP4537/Project/Client/js/tracking.js
+++ COMP4537/Project/Client/js/tracking.js
@@ -9,113 +9,6 @@ $(document).ready(function () {
     console.log(user);
     $("#loginUserName").html(user.user_name);
 
-    //add flight
-    $("#add_flight_submit").click(() => {
-        fetch('https://covid-flight-backend.herokuapp.com/v1/createFlightEntry', {
-            method: 'POST', // likewise we have DELETE, PUT, PATCH
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                flight_id: document.getElementById('add_flight_id').value,
-                flight_date: document.getElementById('add_flight_date').value,
-                to_city: document.getElementById('add_to_city').value,
-                from_city: document.getElementById('add_from_city').value,
-                flight_company: document.getElementById('add_flight_company').value
-            })
-        }).
-        then(res => {
-                console.log(res.json);
-                if (res.status == 200) {
-                    console.log("Success");
-                    return res.json();
-
-                } else if (res.status == 401) {
-                    throw new Error('Invalid Values');
-                } else {
-                    console.log(res.json);
-                }
-            })
-            .then(data => {
-                alert("Data Entry Success.");
-            }).
-        catch(e => {
-            // alert(e)
-        });
-
-    });
-
-    //delete flight
-    $("#delete_flight_submit").click(() => {
-        fetch('https://covid-flight-backend.herokuapp.com/v1/deleteFlightEntry', {
-            method: 'DELETE', // likewise we have DELETE, PUT, PATCH
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                flight_id: document.getElementById('flight_id').value,
-                flight_date: document.getElementById('flight_date').value
-            })
-        }).
-        then(res => {
-                console.log(res.json);
-                if (res.status == 200) {
-                    console.log("Success");
-                    return res.json();
-
-                } else if (res.status == 401) {
-                    throw new Error('Invalid Values');
-                } else {
-                    console.log(res.json);
-                }
-            })
-            .then(data => {
-                alert("Delete Success.");
-            }).
-        catch(e => {
-            // alert(e)
-        });
-    });
-
-
-    //edit flight
-    $("#edit_flight_submit").click(() => {
-        fetch('https://covid-flight-backend.herokuapp.com/v1/updateFlightEntry', {
-            method: 'PUT', // likewise we have DELETE, PUT, PATCH
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                flight_id: '\''+document.getElementById('edit_flight_id').value + '\'',
-                flight_date: document.getElementById('edit_flight_date').value,
-                to_city: document.getElementById('edit_to_city').value,
-                from_city: document.getElementById('edit_from_city').value,
-                flight_company: document.getElementById('edit_flight_company').value
-            })
-        }).
-        then(res => {
-                console.log(res.json);
-                if (res.status == 200) {
-                    console.log("Success");
-                    return res.json();
-
-                } else if (res.status == 401) {
-                    throw new Error('Invalid Values');
-                } else {
-                    console.log(res.json);
-                }
-            })
-            .then(data => {
-                alert("Update Success.");
-            }).
-        catch(e => {
-            // alert(e)
-        });
-    });
-
       //display all flights
       $("#displayAll").click(() => {
         fetch('https://covid-flight-backend.herokuapp.com/v1/getFlights', {
@@ -162,5 +55,49 @@ $(document).ready(function () {
         window.location.href = './login.html';
     });
 
+
+        $('#search_flight_submit').click(()=> {
+
+            if (!$('#flight_id').val()){
+                alert("You have to enter flight number");   
+            }
+            else if (!$('#flight_date').val()){
+                alert("You have to enter flight date");   
+            }
+            else {
+                fetch("https://covid-flight-backend.herokuapp.com/v1/getSingleFlight/'" + $('#flight_id').val() + "'/'" + $('#flight_date').val() + "'", {
+                    method: 'GET', // likewise we have DELETE, PUT, PATCH
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    }
+                }).
+                then(res => {
+                    if (res.status == 200) {
+                        console.log("Success");
+                        return res.json();
+                    } else if (res.status == 401) {
+                        throw new Error('Invalid Values');
+                    }else{
+                        console.log(res.json);
+                    }
+                })
+                .then(data => {
+                    if(data){
+                        let txt = "<b>Here's brief information about " + $('#flight_id').val() + " on " + $('#flight_date').val() + '</b>';
+                        txt += "<br/><br/>Flight Company: " + data.flight_company;
+                        txt += "<br/>Departure: " + data.from_city;
+                        txt += "<br/>Arrival: " + data.to_city;
+                        $('#result').html(txt);
+                    }
+                    else {
+                        $('#result').html('<b>Unable to find the flight information.</b>');
+                    }
+                })
+                .catch(e => {
+                    // alert(e)
+                });
+            }
+        })
 
 });
